5059a69628c41712738b74a7f51e16f9fb2b0305
HoriunovaM448 <margarita28028920@gmail.com>
2020-12-10 12:33:08 +0300

    added index

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+   $('.slider').slick({
+      arrows: false,
+      fade: true,
+      autoplay: 3000,
+      dots: true
+   });
+
+   $('.header__burger').click(function (event) {
+      $('.header__burger,.header__menu').toggleClass('active');
+   });
+
+
+   $("#header__menu, #top").on("click", "a", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+      //забираем идентификатор бока с атрибута href
+
+      var id = $(this).attr('href'),
+         //узнаем высоту от начала страницы до блока на который ссылается якорь
+         top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+
+      $('body,html').animate({ scrollTop: top }, 1500);
+   });
+
+
+
+
+});
